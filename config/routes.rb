b0a0061e823d99e3721cbaa5392cf063cd840c1b
LGM1213 <lgm1213@gmail.com>
2017-01-18 21:14:34 -0500

    update routes to landing#index and migrated models

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :cleaners
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "landings#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119014330) do
+ActiveRecord::Schema.define(version: 20170119015253) do
 
   create_table "cleaners", force: :cascade do |t|
     t.string   "username"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20170119014330) do
     t.string   "city"
     t.string   "state"
     t.string   "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "landings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
